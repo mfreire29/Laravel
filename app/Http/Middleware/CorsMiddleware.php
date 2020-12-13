7d5f8535bdb44f74d686d54c7fed45091bba9627
Mauro Freire <mfreire@bief.com.ar>
2020-12-13 01:38:32 -0300

    Cambio para Cors App Deployed

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,7 +15,7 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin'      => 'https://competent-fermat-7b35d4.netlify.app',
+            'Access-Control-Allow-Origin'      => 'https://stupefied-shirley-9c875a.netlify.app',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
